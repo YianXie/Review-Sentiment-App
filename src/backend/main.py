7ab61879d05f0b0916b67c370495783dcd0d69af
YianXie <yian.xie@outlook.com>
2026-06-24 18:52:16 -0700

    feat: added backend FRONTEND_URL environmental variable to prevent CORS errors

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from .config import settings
 
 from review_sentiment_app.predict import predict
 
@@ -9,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[settings.FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
